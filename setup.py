1f87606478f847d826950578ee5a4eb0752b8ee7
Bruno Maciel massaini <brunomassaini@gmail.com>
2023-06-13 16:29:00 +0200

    adding icon support

--- setup.py
+++ setup.py
@@ -4,15 +4,20 @@ import os
 
 client_id = os.getenv('DISCORD_CLIENT_ID')
 
+# Icon file name
+#ICON_FILE = 'icon.icns'
+
 # Update the setup configuration
 setup(
     app=['bot/main.py'],
     options={
         'py2app': {
             'argv_emulation': True,
+            'iconfile':'icon.icns',
             'plist': {
                 'CFBundleShortVersionString': '1.0',
                 'CFBundleName': 'AppleMusicDiscordBot',
+                #'CFBundleIconFile': ICON_FILE,
                 'LSEnvironment': {
                     'DISCORD_CLIENT_ID': client_id,
                 },
